307d5d166bf046531903741cffa206525702285f
PyGabba <124427614+PyGabba@users.noreply.github.com>
2026-03-17 13:50:04 +0100

    Implement website reloader with axios
    Added a reloader function to periodically fetch data from the specified URL.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
+const url = `https://poke-whp.onrender.com/`; // Render URL
+const interval = 300000; // Interval in milliseconds (300 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 let menu = null;
 let cart = [];
 let pokeBuilder = { size: 'regular', base: [], proteine: [], fruttaVerdura: [], salse: [], topping: [] };
